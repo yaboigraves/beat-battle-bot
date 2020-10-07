3d3a57ba76c6c09ff66af9429f731ef1d71652e0
reid <2056111+sad@users.noreply.github.com>
2020-10-07 22:57:21 +0100

    flesh out battle command some more

--- src/commands/battles/start.js
+++ src/commands/battles/start.js
@@ -1,10 +1,29 @@
-const { Command } = require('discord-akairo');
+const { Command, Argument } = require('discord-akairo');
+
+// const Battle = require('../../models/battle');
 
 class BattleCommand extends Command {
   constructor() {
     super('battle', {
       aliases: ['battle', 'start'],
       category: 'battles',
+      channel: 'guild',
+      ratelimit: 1,
+      cooldown: 300000,
+      args: [
+        {
+          // time in minutes for the battle to last
+          // 10 minutes to 4 hours
+          // todo: inhibitor for incorrect argument
+          id: 'time',
+          type: Argument.range('number', 10, 240),
+          default: '30',
+        },
+        {
+          id: 'sample',
+          type: 'string',
+        },
+      ],
       description: {
         icon: ':crossed_swords:',
         content: 'Start a beatbattle.',
@@ -13,10 +32,17 @@ class BattleCommand extends Command {
     });
   }
 
-  async exec(message) {
+  async exec(message, { time, sample }) {
     const reactTimeout = 5000; // temporary
     const reactFilter = (reaction, user) => {
       return ['⚔️'].includes(reaction.emoji.name) && user.id !== this.client.user.id;
+    };
+
+    // eslint-disable-next-line no-unused-vars
+    const battleOpts = {
+      serverID: message.guild.id,
+      length: time,
+      sample,
     };
 
     const reactEmbed = this.client.util.embed().setColor('GOLD').setDescription('React to this message to join the battle');
@@ -25,6 +51,8 @@ class BattleCommand extends Command {
       msg.awaitReactions(reactFilter, { time: reactTimeout }).then((collected) => {
         const swords = collected.first().message.reactions.cache;
         message.channel.send(`${swords.first().count - 1} people reacted`);
+        // loop through and push to battleOpts.playerIds = [];
+        // const battle = new Battle(battleOpts);
       });
     });
   }
