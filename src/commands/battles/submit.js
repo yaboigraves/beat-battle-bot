ab43f6447f5c0403fe9fe668af79d8e42ab7307a
Graves <theredhead242@gmail.com>
2020-10-09 15:14:32 -0700

    initial submit argument problem

---
+++ src/commands/battles/submit.js
@@ -0,0 +1,38 @@
+const { Command } = require('discord-akairo');
+
+class SubmitCommand extends Command {
+  constructor() {
+    super('submit', {
+      aliases: ['submit'],
+      category: 'battle',
+      description: {
+        icon: ':small_blue_diamond:',
+        content: 'Submit your beat.',
+        args: [
+          {
+            // time in minutes for the battle to last
+            // 10 minutes to 4 hours
+            // todo: inhibitor for incorrect argument
+            id: 'link',
+            type: 'string',
+            // i got the socc on me
+            default: 'https://www.youtube.com/watch?v=E_Wih7Bgmxo',
+            match: 'rest',
+
+          },
+        ],
+        usage: '.submit [submissionLink]',
+      },
+    });
+  }
+
+  async exec(message, { link }) {
+    // check if there's a battle running in the current server that the user is a participant in
+    console.log(link);
+    // append the submission of that user to the submissions list
+    message.channel.send('Submission Recieved!');
+    console.log(`got submission ${link}`);
+  }
+}
+
+module.exports = SubmitCommand;
