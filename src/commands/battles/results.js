f2b511e4d867f7c561bd37838efca34fcfc2e230
Graves <theredhead242@gmail.com>
2020-10-11 16:59:04 -0700

    results now finds winner from submission

--- src/commands/battles/results.js
+++ src/commands/battles/results.js
@@ -16,13 +16,34 @@ class ResultsCommand extends Command {
 
   async exec(message) {
     // figure out which server battle we're getting the results from
-    Battle.findOne({ serverID: message.guild.id, status: 'VOTING' }).then((serverBattle) => {
-      // loop through all the player ids
+    let winner;
+    await Battle.findOne({ serverID: message.guild.id, status: 'VOTING' }).then((serverBattle) => {
+      winner = {
+        id: serverBattle.playerIDs[0],
+        submissionLink: '',
+        score: 0,
+      };
 
+      // loop through all the player ids
       for (let i = 0; i < serverBattle.playerIDs.length; i += 1) {
         const { submissionsScores } = serverBattle;
-        message.channel.send(submissionsScores[serverBattle.playerIDs[i]]);
+
+        const score = submissionsScores[serverBattle.playerIDs[i]];
+
+        message.channel.send(score);
+
+        // check if the score is larger than the current winners score
+
+        if (winner.score < score) {
+          winner.id = serverBattle.playerIDs[i];
+          winner.submissionLink = serverBattle.submissions[winner.id];
+          winner.score = score;
+        }
       }
+
+      // after the await, we then display the winner and then update leaderboards
+      // TODO: make this prettier
+      message.channel.send(winner.submissionLink);
 
       // TODO: re-enable
       /*
