23a2c4f766a3cacc0d93ed79999baf70ccf5103a
Graves <theredhead242@gmail.com>
2020-10-10 18:04:23 -0700

    basic collector working with loop

--- src/commands/battles/vote.js
+++ src/commands/battles/vote.js
@@ -1,4 +1,5 @@
 const { Command } = require('discord-akairo');
+const battle = require('../../models/battle');
 const Battle = require('../../models/battle');
 
 class VoteCommand extends Command {
@@ -16,49 +17,43 @@ class VoteCommand extends Command {
 
   async exec(message) {
     // check if its voting time (this is flipped after the battle timer is done)
+    let serverBattle;
 
-    Battle.findOne({ serverID: message.guild.id, status: 'VOTING' }).then((serverBattle) => {
+    const filter = (reaction, user) => {
+      return ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣'].includes(reaction.emoji.name) && user.id !== this.client.user.id;
+    };
+
+    Battle.findOne({ serverID: message.guild.id, status: 'VOTING' }).then((battleResults) => {
       // loop through all the playerid's and post their submissions, add 1-5 reactions below them
-
-      const filter = (reaction, user) => {
-        return ['⚔️', '2️⃣', '3️⃣', '4️⃣', '5️⃣'].includes(reaction.emoji.name) && user.id !== this.client.user.id;
-      };
-
-      message.channel.send(serverBattle.submissions[serverBattle.playerIDs[0]])
-        .then((msg) => {
-          // this is to force the order, otherwise it may desync
-
-          // msg.react('⚔️');
-          // msg.react('2️⃣');
-          // msg.react('3️⃣');
-          // msg.react('4️⃣');
-          // msg.react('5️⃣');
-
-          msg.awaitReactions({ filter }, { time: 15000 }).then((collected) => {
-            // go through all the reactions to this vote
-            // keep track of all the userids that have already been logged
-            // (this doesnt need to be stored db side)
-            // if any duplicates come up take the first one that appears and
-            // ignore the rest of the reactions
-            // console.log(collected.size);
-
-            // nobody reacted
-            if (!collected.first()) {
-              const embed = this.client.util.embed()
-                .setColor('RED')
-                .setTitle(':warning: Nobody voted for this beat aww.')
-                .setDescription('To start another battle, use `.battle <sample>`.\nSee `.help battle` for more information.');
-
-              msg.delete();
-              return message.channel.send(embed);
-            }
-          });
-        });
-
+      if (battleResults === null) {
+        return message.channel.send('No battle in the voting phase to vote in buster');
+      }
+      serverBattle = battleResults;
       // this filter will ignore any reactions other than 1,2,3,4,5 and ignores the bots reactions
     });
 
-    // message.channel.send('Voting will end in 10 seconds');
+    const reactEmbed = this.client.util.embed()
+      .setColor('GOLD')
+      .setTitle(':crossed_swords: Voting Time!')
+      .setDescription('Voting time.');
+    for (let i = 0; i < 3; i += 1) {
+      message.channel.send(reactEmbed).then((msg) => {
+        msg.react('1️⃣')
+          .then(() => msg.react('2️⃣')).then(() => {
+            // this will need to be moved to a list or something otherwise
+            // these are broken once we loop
+            const collector = msg.createReactionCollector(filter, { time: 15000 });
+            collector.on('collect', (reaction, user) => {
+              console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
+            });
+
+            collector.on('end', (collected) => {
+              console.log(`Collected ${collected.size} items`);
+              return message.channel.send('voting has ended');
+            });
+          });
+      });
+    }
   }
 }
 
