2d0c63a30ed7f0219f09d5814fa53c264b742369
Graves <theredhead242@gmail.com>
2020-11-02 18:21:39 -0800

    fuck

--- src/commands/general/ytdownload.js
+++ src/commands/general/ytdownload.js
@@ -5,6 +5,8 @@ const logger = require('../../logger');
 
 const dl = new Downloader();
 
+const ytdl = require('ytdl-core');
+
 class YtDownloadCommand extends Command {
   constructor() {
     super('ytDownload', {
@@ -29,13 +31,27 @@ class YtDownloadCommand extends Command {
     const videoid = sample.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
     logger.success('checkpoint 1');
     if (videoid != null) {
-      logger.success(videoid);
+      // going to try a rewrite with just readable streams and the core library
+
+      // const stream = ytdl(sample, {
+      //   encoderArgs: ['-af', 'asetrate=44100*1.25,bass=g=20,dynaudnorm=f=150'],
+      //   fmt: 'mp3',
+      //   opusEncoded: false,
+      // });
+
+      // stream.pipe(fs.createWriteStream('test.mp3'));
+
+      // stream.on('finish', () => {
+      //   message.channel.send('', { files: [{ attachment: './test.mp3' }] });
+      // });
+
       dl.getMP3({ videoId: videoid[1], serverId: message.guild.id }, (err, res) => {
         if (err) {
           logger.success('checkpoint error ');
           throw err;
         } else {
           logger.success('made it to the send part');
+          logger.success(res);
           message.channel.send('', { files: [{ attachment: res.file, name: `${res.videoTitle}.mp3` }] }).then(() => {
             if (fs.existsSync(res.file)) {
               fs.unlink(res.file, (errr) => {
